9f62fcdf83bf6c6c232f0a55b55c64e1d0471ac1
Thitv_dev <thitv.hue@vnpt.vn>
2023-03-03 11:38:08 +0700

    Upload Config

---
+++ API/API_TPL/Controllers/Danhmuc/PhanxuongController.cs
@@ -0,0 +1,52 @@
+﻿using API_TPL.DAL;
+using Microsoft.AspNet.Identity;
+using Oracle.ManagedDataAccess.Client;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+
+
+namespace API_TPL.Controllers.Danhmuc
+{
+    //[Authorize]
+    [RoutePrefix("api/dmphanxuong")]
+    public class PhanxuongController : ApiController
+    {
+        static String connString = ConfigurationManager.ConnectionStrings["PHANBONConnection"].ToString();
+        SQL_DBHELPERs helper = new SQL_DBHELPERs(connString);
+
+        ///<summary>
+        ///<b>Mục đích:</b>Lấy danh sách phân xưởng. <br />
+        ///<b>Tham số URI:</b> Không có. <br />
+        ///<b>Trả về:</b> Datatable <br />
+        ///</summary>
+        [HostAuthentication(DefaultAuthenticationTypes.ExternalBearer)]
+        [Route("getall"), HttpGet]
+        public IHttpActionResult getAll()
+        {
+            string query_str = "phanxuong_getall";
+
+            object[] aParams = new object[0];
+            try
+            {
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+        }
+    }
+}
---
+++ API/API_TPL/DAL/SQL_DBHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace API_TPL.DAL
+{
+    /// <summary>
+    /// connect sql
+    /// </summary>
+    public class SQL_DBHELPERs
+    {
+        SqlConnection conn;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SQL_DBHELPERs(string connString)
+        {
+            conn = new SqlConnection(connString);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="query_string"></param>
+        /// <param name="inParams"></param>
+        /// <returns></returns>
+        public DataTable ExecuteQueryStoreProcedure(string query_string, object[] inParams)
+        {
+            try
+            {
+                DataTable data = new DataTable();
+                SqlCommand cmd;
+
+                conn.Open();
+
+                cmd = conn.CreateCommand();
+                cmd.CommandText = query_string;
+                cmd.CommandType = CommandType.StoredProcedure;
+                if (inParams != null)
+                    cmd.Parameters.AddRange(inParams);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(data);
+
+                conn.Close();
+                return data;
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                throw ex;
+
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SqlParameter BuildParameter(string name, object value, SqlDbType dbtype)
+        {
+            SqlParameter param = new SqlParameter(name, dbtype);
+            param.Value = value;
+            return param;
+        }
+    }
+}
