8eb6a85517a77391fb9e596004a75e8cfd9a2498
Thitv_dev <thitv.hue@vnpt.vn>
2023-03-20 19:23:47 +0700

    Update

---
+++ API/API_TPL/Controllers/Nghiepvu/NhapkhoController.cs
@@ -0,0 +1,137 @@
+﻿using API_TPL.DAL;
+using Microsoft.AspNet.Identity;
+using Oracle.ManagedDataAccess.Client;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+
+
+namespace API_TPL.Controllers.Danhmuc
+{
+    //[Authorize]
+    [RoutePrefix("api/nhapkho")]
+    public class NhapkhoController : ApiController
+    {
+        static String connString = ConfigurationManager.ConnectionStrings["PHANBONConnection"].ToString();
+        SQL_DBHELPERs helper = new SQL_DBHELPERs(connString);
+
+        ///<summary>
+        ///<b>Mục đích:</b>Lấy danh sách phân xưởng. <br />
+        ///<b>Tham số URI:</b> Không có. <br />
+        ///<b>Trả về:</b> Datatable <br />
+        ///</summary>
+        [HostAuthentication(DefaultAuthenticationTypes.ExternalBearer)]
+        [Route("getall"), HttpGet]
+        public IHttpActionResult getAll()
+        {
+            string query_str = "dm_vattu_getall";
+
+            object[] aParams = new object[0];
+            try
+            {
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+        }
+
+        [Route("nhapkho"), HttpPost]
+        public IHttpActionResult DM_VATTU([FromBody] dynamic obj)
+        {
+            string query_str = "themmoi_vattu_nguyenlieu_kho";
+
+            object[] aParams = new object[1];
+            try
+            {
+                aParams[0] = helper.BuildParameter("data", obj.data, System.Data.SqlDbType.NVarChar);
+
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                //return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+                string err = ex.Message.Substring(0, ex.Message.IndexOf("\n", 0)).Substring(ex.Message.IndexOf(":") + 2).Trim();
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, err));
+            }
+        }
+        [Route("xoa"), HttpPost]
+        public IHttpActionResult KHO_VATTU_XOA([FromBody] dynamic obj)
+        {
+            string query_str = "xoavattukho";
+
+            object[] aParams = new object[1];
+            try
+            {
+                aParams[0] = helper.BuildParameter("ma_vattu", obj.ma_vattu, System.Data.SqlDbType.NVarChar);
+
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                //return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+                string err = ex.Message.Substring(0, ex.Message.IndexOf("\n", 0)).Substring(ex.Message.IndexOf(":") + 2).Trim();
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, err));
+            }
+        }
+        [Route("getbymakho"), HttpPost]
+        public IHttpActionResult NGUYENLIEU_VATTU_KHO([FromBody] dynamic obj)
+        {
+            string query_str = "lay_ds_nguyenlieu_vattu_kho";
+
+            object[] aParams = new object[1];
+            try
+            {
+                aParams[0] = helper.BuildParameter("ma_kho", obj.ma_kho, System.Data.SqlDbType.NVarChar);
+
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                //return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+                string err = ex.Message.Substring(0, ex.Message.IndexOf("\n", 0)).Substring(ex.Message.IndexOf(":") + 2).Trim();
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, err));
+            }
+        }
+        [Route("getbyloaivt"), HttpPost]
+        public IHttpActionResult DM_VATTU_BYLOAIVT([FromBody] dynamic obj)
+        {
+            string query_str = "vattu_theoloaivattu";
+
+            object[] aParams = new object[1];
+            try
+            {
+                aParams[0] = helper.BuildParameter("loai_vattu", obj.loai_vattu, System.Data.SqlDbType.Int);
+
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                //return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+                string err = ex.Message.Substring(0, ex.Message.IndexOf("\n", 0)).Substring(ex.Message.IndexOf(":") + 2).Trim();
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, err));
+            }
+        }
+    }
+}
